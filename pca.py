293a9750400ce84b6ad565ce5eb07c407c177f79
orshemesh <chikos2011@gmail.com>
2020-07-29 17:41:50 +0300

    pca algorithm

---
+++ pca.py
@@ -0,0 +1,102 @@
+# Principal Component Analysis (PCA)
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from parser_data import writer , parser
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+from matplotlib.colors import ListedColormap
+
+#read and randomization corona file
+writer(parser())
+dataset = pd.read_csv('dataset/corona.csv')
+dataset=dataset[2000:3000]
+
+#Split the data into features and classification
+X = dataset.iloc[:, [0, 1]].values
+y = dataset.iloc[:, 2].values
+
+# Splitting the dataset into the Training set 75% and Test set 25%
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+# Feature Scaling , Transformation
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Applying PCA function on training
+# and testing set of X component
+pca = PCA(n_components = 2)
+
+# Feature Scaling , Transformation
+X_train = pca.fit_transform(X_train)
+X_test = pca.transform(X_test)
+
+explained_variance = pca.explained_variance_ratio_
+
+# Fitting Logistic Regression to the Training set
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+#Calculation of accuracy score
+print("accuracy score:  " , classifier.score(X_train, y_train))
+
+# Visualising the Training set results
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1,
+                     stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1,
+                     stop = X_set[:, 1].max() + 1, step = 0.01))
+
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(),
+             X2.ravel()]).T).reshape(X1.shape), alpha = 0.75,
+             cmap = ListedColormap(('blue','red')))
+
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('blue','red'))(i), label = j)
+
+plt.title('Principal Component Analysis (Training set)')
+plt.xlabel('PC1-deaths')
+plt.ylabel('PC2-confirmed')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1,
+                     stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1,
+                     stop = X_set[:, 1].max() + 1, step = 0.01))
+
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(),
+             X2.ravel()]).T).reshape(X1.shape), alpha = 0.75,
+             cmap = ListedColormap(('blue','red')))
+
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('blue','red'))(i), label = j)
+
+plt.title('Principal Component Analysis (Test set)')
+plt.xlabel('PC1-deaths')
+plt.ylabel('PC2-confirmed')
+plt.legend()
+plt.show()
